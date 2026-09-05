fdd882283daafacf7bde7b26cf75cdcfdec26651
vaibhavi014 <sharmavaibhavivs10@gmail.com>
2026-09-05 23:13:27 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from finflux.controllers.api import api_bp
-from finflux.controllers.cdc_consumer import run_cdc_loop
-from finflux.controllers.dashboard import dashboard_bp
-from finflux.controllers.health import health_bp
